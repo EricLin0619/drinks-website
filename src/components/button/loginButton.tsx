586ca602ce1ad3af2445e06be39b21ecae688f82
briankuo123 <brian.eh.kuo@gmail.com>
2023-11-28 15:29:51 +0800

    do not push

--- src/components/button/loginButton.tsx
+++ src/components/button/loginButton.tsx
@@ -1,9 +1,14 @@
 'use client';
+import axios from 'axios';
 export default function LoginButton() {
   const handleButtonClick = async () => {
     if (document) {
       (document.getElementById("login_modal") as HTMLFormElement).showModal();
     }
+  }
+
+  function login() {
+
   }
 
   return (
@@ -21,7 +26,7 @@ export default function LoginButton() {
           <h3 className="font-bold text-lg text-center">請輸入您註冊的電子郵件及密碼</h3>
           <input type="text" placeholder="電子郵件帳號" className="input input-bordered w-full bg-white mt-4 text-white"/>
           <input type="text" placeholder="密碼" className="input input-bordered w-full bg-white mt-4 text-white"/>
-          <button className="btn btn-success mt-4 w-full">登入</button>
+          <button className="btn btn-success mt-4 w-full" onClick={() => alert("登入")}>登入</button>
         </div>
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
