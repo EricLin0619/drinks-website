a141048029ef2018b8297fe76aa648eb9ea829b2
Eric Lin <40213852eric@gmail.com>
2023-12-06 18:51:00 +0800

    feat: add drinks and coupon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -49,6 +49,7 @@ export default function Navbar() {
             <img src="./shopping-cart.png" alt="shopping-cart" className='w-8 h-8 cursor-pointer' onClick={()=>{router.push("./shoppingCart")}}/>
             <img src="./user.png" alt="shopping-cart" className='w-7 h-7 cursor-pointer ml-4' onClick={()=>{router.push("./userData")}}/>
             <img src="./user.png" alt="shopping-cart" className='w-7 h-7 cursor-pointer ml-4' onClick={()=>{router.push("./Userhistory")}}/>
+            <img src="./store.png" alt="shopping-cart" className='w-7 h-7 cursor-pointer ml-4' onClick={()=>{router.push("./store")}}/>
           </>
           :
           <>
