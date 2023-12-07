9ea45db3284629d8f68866d4ad9c2ffc5f320891
Eric Lin <40213852eric@gmail.com>
2023-12-07 10:44:16 +0800

    feat: currentOrderCard

--- src/type.ts
+++ src/type.ts
@@ -1,27 +1,37 @@
 export type AddDrinkButtonType = {
-    name: string;
-    price: number;
-    imageUrl: string;
-}
+  name: string;
+  price: number;
+  imageUrl: string;
+};
 
 export type DrinkCardType = {
-    name: string;
-    price: number;
-    imageUrl: string;
-    description: string;
-}
+  name: string;
+  price: number;
+  imageUrl: string;
+  description: string;
+};
 
 export type UnaccptedOrderCardType = {
-    orderNumber: string;
-    phone: string;
-    customerName: string;
-    method: string;
-    content: Array<any>
-}
+  orderNumber: string;
+  phone: string;
+  customerName: string;
+  method: string;
+  content: Array<any>;
+};
 
 export type AlterDrinksType = {
-    name: string;
-    price: number;
-    imageUrl: string;
-    description: string;
-}
+  name: string;
+  price: number;
+  imageUrl: string;
+  description: string;
+};
+
+export type HistoryOrderCardType = {
+  time: string;
+  drinks: Array<any>;
+};
+
+export type CurrentOrderCardType = {
+  time: string;
+  drinks: Array<any>;
+};
