9e0d06e3b7d53994e3dca7c2270f1a62d145ef56
jack90325 <jack90325@winland.com.tw>
2023-12-20 01:31:49 +0800

    orderManagement_update

---
+++ src/app/orderManagement/components/OrderCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import axios from "axios";
+import { OrderCardType} from "@/type";
+import { useEffect, useState } from "react";
+
+function OrderCard(props: OrderCardType) {
+  const jwtToken = sessionStorage.getItem('jwt')
+  const [drinkName, setDrinkName] = useState("");
+  const [drinkPrice, setDrinkPrice] = useState("");
+  useEffect(() => {
+    props.menu.forEach((drink) => {
+      if(drink.drinkId === props.drinkId){
+        setDrinkName(drink.drinkName);
+        setDrinkPrice(drink.drinkPrice);
+      }
+    });
+  },[])
+  return (
+    <div className="flex">
+      <p>1x</p>
+      <div className="ml-2">
+        <p>{drinkName}</p>
+        <p className="text-slate-400">
+          {props.sugar}
+          {props.ice}
+        </p>
+      </div>
+      <p className="ml-auto">$ {drinkPrice}</p>
+    </div>
+  );
+}
+export default OrderCard;
